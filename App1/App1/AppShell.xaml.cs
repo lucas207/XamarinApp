e4bde23bc84f1ef519749e2dfc71c353f225c32e
Dancrash <lucas.nota10@hotmail.com>
2020-11-22 21:49:01 -0300

    first

---
+++ App1/App1/AppShell.xaml.cs
@@ -0,0 +1,28 @@
+﻿using App1.ViewModels;
+using App1.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+
+        private async void Spam(object sender, EventArgs e)
+        {
+            await DisplayAlert("Alert", "You have been alerted", "OK");
+        }
+    }
+}
---
+++ App1/App1/Views/AboutPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+
+namespace App1.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+        
+
+        private async void OpenCamera(object sender, EventArgs e)
+        {
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("Nenhuma Câmera", ":( Nenuma Câmera disponível.", "OK");
+                return;
+            }
+
+            var armazenamento = new StoreCameraMediaOptions()
+            {
+                SaveToAlbum = true,
+                Name = "MinhaFoto.jpg"
+            };
+            var foto = await CrossMedia.Current.TakePhotoAsync(armazenamento);
+
+            if (foto == null)
+                return;
+            imgFoto.Source = ImageSource.FromStream(() =>
+            {
+                var stream = foto.GetStream();
+                foto.Dispose();
+                return stream;
+            });
+        }
+
+        void LogoTapped(object sender, EventArgs args)
+        {
+            try
+            {
+                DisplayAlert("Alert", "Até um outro dia", "OK");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private async void SelecionarImagem(object sender, EventArgs e)
+        {
+            if (CrossMedia.Current.IsTakePhotoSupported)
+            {
+                var imagem = await CrossMedia.Current.PickPhotoAsync();
+                if (imagem != null)
+                {
+                    imgFoto.Source = ImageSource.FromStream(() =>
+                    {
+                        var stream = imagem.GetStream();
+                        imagem.Dispose();
+                        return stream;
+                    });
+                }
+            }
+        }
+    }
+}
